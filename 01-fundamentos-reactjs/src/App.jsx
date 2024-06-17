722f8ac7b868843157a700123ad73495c022d7c2
Leonardo Braga <programer_frontbraga@outlook.com>
2024-06-16 21:04:08 -0300

    components

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,17 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+}
