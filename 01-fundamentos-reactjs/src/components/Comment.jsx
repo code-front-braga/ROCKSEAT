3d2619b50d304f489d005fbab05ea9c76a1222d6
Leonardo Braga <programer_frontbraga@outlook.com>
2024-06-18 20:52:40 -0300

    add file Avatar and styling

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,11 +2,12 @@ import { Trash } from 'phosphor-react';
 import { ThumbsUp } from 'phosphor-react';
 
 import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src='https://github.com/code-front-braga.png' />
+      <Avatar hasBorder={false} src='https://github.com/code-front-braga.png' />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
