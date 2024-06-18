0cc5cf14d86b037b812ac76554886fd1ca217f71
Leonardo Braga <programer_frontbraga@outlook.com>
2024-06-18 14:59:24 -0300

    add sidebar file and styling

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,22 +1,29 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <>
       <Header />
 
-      <Post
-        author='Leonardo Braga'
-        content='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, illum!'
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Leonardo Braga'
+            content='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, illum!'
+          />
 
-      <Post
-        author='Adailza Braga'
-        content='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, illum!'
-      />
+          <Post
+            author='Adailza Braga'
+            content='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, illum!'
+          />
+        </main>
+      </div>
     </>
   );
 }
