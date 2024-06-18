f3d7ea2a99145595f9e25920af38439f8621d40b
Leonardo Braga <programer_frontbraga@outlook.com>
2024-06-18 15:25:56 -0300

    install phosphor icons and styling

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,17 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src='https://github.com/code-front-braga.png' />
+
         <strong>Leonardo Primo</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href='#'>Editar seu perfil</a>
+        <a href='#'>
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
