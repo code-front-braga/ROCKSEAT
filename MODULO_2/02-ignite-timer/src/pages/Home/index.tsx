ca0f16ae5213ddf405cda7693302804f1071c18b
Leonardo Braga <programer_frontbraga@outlook.com>
2024-07-13 13:00:33 -0300

    interrupt cycle

--- MODULO_2/02-ignite-timer/src/pages/Home/index.tsx
+++ MODULO_2/02-ignite-timer/src/pages/Home/index.tsx
@@ -31,6 +31,7 @@ interface Cycle {
   task: string;
   minutesAmount: number;
   startDate: Date;
+  interruptedDate?: Date;
 }
 
 export function Home() {
@@ -80,6 +81,19 @@ export function Home() {
     reset();
   }
 
+  function handleInterruptCycle() {
+    setCycles(
+      cycles.map(cycle => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interruptedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      })
+    );
+    setActiveCycleId(null);
+  }
+
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
@@ -107,6 +121,7 @@ export function Home() {
             id='task'
             list='task-suggestions'
             placeholder='Dê um nome para o seu projeto'
+            disabled={!!activeCycle}
             {...register('task')}
           />
 
@@ -122,9 +137,10 @@ export function Home() {
             type='number'
             id='minutesAmount'
             placeholder='00'
-            step={10}
+            step={5}
             min={5}
             max={60}
+            disabled={!!activeCycle}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
 
@@ -140,15 +156,15 @@ export function Home() {
         </CountdownContainer>
 
         {activeCycle ? (
+          <StopCountdownButton onClick={handleInterruptCycle} type='button'>
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
           <StartCountdownButton disabled={isSubmitDisabled} type='submit'>
             <Play size={24} />
             Começar
           </StartCountdownButton>
-        ) : (
-          <StopCountdownButton type='button'>
-            <HandPalm size={24} />
-            Começar
-          </StopCountdownButton>
         )}
       </form>
     </HomeContainer>
