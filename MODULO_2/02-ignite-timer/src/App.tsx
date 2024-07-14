5e5ca2a520bffba022bbd42a9a41620d12647e26
Leonardo Braga <programer_frontbraga@outlook.com>
2024-07-14 13:45:52 -0300

    useContext

--- MODULO_2/02-ignite-timer/src/App.tsx
+++ MODULO_2/02-ignite-timer/src/App.tsx
@@ -1,15 +1,21 @@
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
-
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
 import { Router } from './Router';
 
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+import { CyclesContextProvider } from './contexts/CyclesContext';
+// import { Home } from './HomeTest';
+
 export function App() {
+  // return <Home />
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
